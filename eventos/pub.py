26f3b237734d99add2510169b81deefafae167b1
DreaMagici4n <lucascostamr812@gmail.com>
2024-10-29 20:35:33 -0300

    temp: change pub

--- eventos/pub.py
+++ eventos/pub.py
@@ -1,27 +1,27 @@
 #simulator device 1 for mqtt message publishing
-import paho.mqtt.client as paho
+import paho.mqtt.client as mqtt
 import time
 import random
 #hostname
-broker="localhost"
+broker="mqtt"
 #port
 port=1883
 
 def on_publish(client, userdata, result):
     print("Dispositivo 1: Dados Publicados.")
     pass
-    
-client= paho.Client("admin")
+
+client= mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_publish = on_publish
 client.connect(broker,port)
 
 for i in range(20):
     d=random.randint(1,5)
-    
+
     #criando mensagem
     message="Dispositivo 1 : Dados " + str(i)
     time.sleep(d)
-    
+
     #publicando mensagem
     ret= client.publish("/data",message)
 print("Parou...")
