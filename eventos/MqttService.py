420f63db62fcfeb160ff178849457ca6a129aca7
lucascostamr <lucascostamr812@gmail.com>
2024-11-02 22:36:13 -0300

    refactor: add MqttSevice

---
+++ eventos/MqttService.py
@@ -0,0 +1,28 @@
+from typing import Callable
+
+import paho.mqtt.client as mqtt
+
+
+class  MqttService:
+    def __init__(self):
+        self.broker = "mqtt"
+        self.port = 1883
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    
+    def connect(self) -> None:
+        self.client.connect(self.broker, self.port)
+    
+    def setPublishHandler(self, publishHandler: Callable):
+        self.client.on_publish = publishHandler
+
+    def setOnConnectHandler(self, onConnectHandler: Callable):
+        self.client.on_connect = onConnectHandler
+
+    def setOnMessageHandler(self, onMessageHandler: Callable):
+        self.client.on_message = onMessageHandler
+
+    def publish(self, topic: str, message: str):
+        self.client.publish(topic, message)
+
+    def listen(self):
+        self.client.loop_forever()
