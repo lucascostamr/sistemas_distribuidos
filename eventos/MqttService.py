5578941316b71d0b973597cbef686fb4d98a7ab4
lucascostamr <lucascostamr812@gmail.com>
2024-11-04 19:23:06 -0300

    refactor: ensure MqttService publish and subscribes

--- eventos/MqttService.py
+++ eventos/MqttService.py
@@ -4,7 +4,6 @@ import paho.mqtt.client as mqtt
 
 
 class  MqttService:
-    ballList = []
     def __init__(self):
         self.broker = "mqtt"
         self.port = 1883
@@ -28,18 +27,3 @@ class  MqttService:
     def listen(self, topic: str):
         self.client.subscribe(topic)
         self.client.loop_forever()
-
-
-if __name__ == "__main__":
-    def on_publish(client, userdata, flags, reason_code, properties):
-        print(properties)
-        MqttService.ballList.append("data")
-        print("Dispositivo 1: Dados Publicados.")
-        pass
-    mqttService = MqttService()
-    mqttService.setPublishHandler(on_publish)
-    mqttService.connect()
-    mqttService.publish("/data", "message")
-    mqttService.publish("/data", "message")
-
-    print(MqttService.ballList)
