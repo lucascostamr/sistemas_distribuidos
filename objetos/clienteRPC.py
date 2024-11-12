b67401aad7f953ed518a2c2c41afd150e5be4101
DreaMagici4n <lucascostamr812@gmail.com>
2024-11-12 20:30:35 -0300

    feat: add user to room and return room content

--- objetos/clienteRPC.py
+++ objetos/clienteRPC.py
@@ -3,6 +3,9 @@ import rpyc
 proxy = rpyc.connect('localhost', 18861)
 
 username = input("Digite seu nome: ")
+proxy.root.register_user(username)
 proxy.root.enter_room(username)
 lista = proxy.root.get_user_list()
+room = proxy.root.get_room()
 print(lista)
+print(room)
--- objetos/servidorRPC.py
+++ objetos/servidorRPC.py
@@ -14,22 +14,33 @@ class User:
 
 
 class ChatService(Service):
-    exposed_user_list: list[User] = []
+    exposed_user_dict: dict[str, User] = {}
+    exposed_room: list[User] = []
+
+    def exposed_register_user(self, username: str) -> None:
+        new_user = {
+            username: User (
+                id=str(uuid1()),
+                name=username
+            )
+        }
+
+        ChatService.exposed_user_dict.update(new_user)
 
     def exposed_enter_room(self, username: str) -> None:
-        new_user = User(id=str(uuid1()), name=username)
-        ChatService.exposed_user_list.append(new_user)
+        user = ChatService.exposed_user_dict[username]
+        ChatService.exposed_room.append(user)
 
     def exposed_get_user_list(self) -> list[User]:
-        return ChatService.exposed_user_list
+        return list(ChatService.exposed_user_dict.values())
+
+    def exposed_get_room(self) -> list[User]:
+        return ChatService.exposed_room
 
 
 job = ThreadedServer(
     ChatService,
-    port=18861,
-    protocol_config={
-        "allow_public_attrs": True,
-    },
+    port=18861
 )
 
 job.start()
